b1dfe123e4049b282fae77e2023cb6fd1eed574f
Zhiyuanlu000217 <lu000217@algonquinlive.com>
2025-06-12 18:13:11 -0400

    chore: modify automated testing

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "07de8b92-8a0e-462b-9b32-7bfdaf51200e"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "lu000217",
 		},
 	}
 
@@ -33,4 +33,31 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Test NIC connection
+	t.Run("Verify NIC Connection", func(t *testing.T) {
+		// Get the VM details
+		vm := azure.GetVirtualMachine(t, vmName, resourceGroupName, subscriptionID)
+		
+		// Verify that the VM has at least one network interface
+		assert.NotEmpty(t, *vm.NetworkProfile.NetworkInterfaces, "VM should have at least one network interface")
+		
+		// Get the first NIC ID and name
+		nicID := *(*vm.NetworkProfile.NetworkInterfaces)[0].ID
+		nicName := azure.GetNameFromResourceID(nicID)
+		
+		// Verify that the NIC exists
+		assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID), "Network interface should exist")
+	})
+
+	// Test Ubuntu version
+	t.Run("Verify Ubuntu Version", func(t *testing.T) {
+		// Get the VM details
+		vm := azure.GetVirtualMachine(t, vmName, resourceGroupName, subscriptionID)
+		
+		// Verify the image reference
+		assert.Equal(t, "Canonical", *vm.StorageProfile.ImageReference.Publisher, "Image publisher should be Canonical")
+		assert.Equal(t, "0001-com-ubuntu-server-jammy", *vm.StorageProfile.ImageReference.Offer, "Image offer should be Ubuntu Server 22.04 LTS")
+		assert.Equal(t, "22_04-lts-gen2", *vm.StorageProfile.ImageReference.Sku, "Image SKU should be 22.04 LTS Gen2")
+	})
 }
